fcf1f1f39073298cb64c54f3bc629216ac408538
spencerfitch <sfitch16@gmail.com>
2021-05-29 01:48:07 -0500

    Basic interfacing between client and server

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,53 @@
 import React from 'react';
 import './App.css';
 
+const baseUrl = "http://localhost:8081"
 
 class App extends React.Component {
-  render () {
-    return (<div>Hello World!</div>)
-  }
+    constructor (props) {
+        super (props);
+        this.state = {
+            "lat": null,
+            "lon": null
+        }
+        this.locationSuccess = this.locationSuccess.bind(this);
+    }
+
+    render () {
+        return (
+            <div>
+                <h2>Current Location</h2>
+                <p><b>Latitude:</b>  {this.state.lat}</p>
+                <p><b>Longitude:</b> {this.state.lon}</p>
+            </div>
+        )
+    }
+
+    componentDidUpdate () {
+        if (this.state.lat && this.state.lon) {
+            fetch(`${baseUrl}/nearest?lat=${this.state.lat}&lon=${this.state.lon}`)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                })
+        }
+    }
+
+    componentDidMount () {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this.locationSuccess)
+        } else {
+            console.log("Geolocation is not supported by this browser");
+        }
+    }
+
+    locationSuccess (position) {
+        this.setState({
+            "lat": position.coords.latitude,
+            "lon": position.coords.longitude
+        });
+        return;
+    }
 }
 
 
--- server/src/routes.js
+++ server/src/routes.js
@@ -11,4 +11,76 @@ router.route("/")
         });
     });
 
+const validCoordinates = (lat, lon) => {
+    /*
+    Check validation of the provided coordinates
+
+    Arguments:
+        lat - provided latitude
+        lon - provided longitude
+
+    Returns:
+        valid (boolean)  - were the coordinates valid?
+        body  (JSON)     - error response body if invalid, lat-lon if valid
+    */
+
+    // Coordinates included?
+    if (!(lat && lon)) {
+        const body = {
+            message: "Must include both the latitude (lat) and longitude (lon) in the query parameters"
+        }
+        return [false, body];
+    } 
+
+    // Coordinates numeric?
+    if (isNaN(lat) || isNaN(lon)) {
+        const body = {
+            message: "Included latitude (lat) and longitude (lon) must be numeric",
+            request: {
+                lat: lat,
+                lon: lon
+            }
+        }
+        return [false, body]
+    }
+
+    const latNumber = Number(lat);
+    const lonNumber = Number(lon);
+    // Coordinates in valid range?
+    if (!(latNumber >= -90 && latNumber <= 90) || 
+        !(lonNumber >= -180 && lonNumber <= 180)) {
+            const body = {
+                message: "Included latitutude must in range -90 to 90 degrees and longitude in range -180 to 180 degrees",
+                request: {
+                    lat: latNumber,
+                    lon: lonNumber
+                }
+            }
+            return [false, body];
+        }
+
+    // Valid coordinates provided
+    const body = {
+        lat: latNumber,
+        lon: lonNumber
+    }
+    return [true, body]
+}
+
+router.route("/nearest")
+    .get((req, res) => {
+        console.log(`GET /nearest?lat=${req.query.lat}&lon=${req.query.lon}`);
+
+        const valid = validCoordinates(req.query.lat, req.query.lon);
+
+        if (!valid[0]) {
+            // Invalid coordinates provided
+            res.status(400).send(valid[1]);
+            return;
+        }
+
+        res.status(200).send(valid[1]);
+        return;
+    })
+
 module.exports = router;
