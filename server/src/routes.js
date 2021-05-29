645e9836809463d08cddbb6bc5a14d607795c82c
spencerfitch <sfitch16@gmail.com>
2021-05-29 03:30:59 -0500

    Retreives nearest plane information and repackages information to JSON

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,7 +1,11 @@
 "use strict";
 
 const express = require("express");
+const axios = require("axios");
+const axiosRetry = require('axios-retry');
 const router = express.Router();
+
+const openSky = axios.create({baseURL: "https://opensky-network.org/api" });
 
 router.route("/")
     .get((req, res) => {
@@ -67,6 +71,30 @@ const validCoordinates = (lat, lon) => {
     return [true, body]
 }
 
+const argMin = (arr, func) => {
+    if (!func) {
+        func = (elem) => elem;
+    }
+    return arr
+            .map((elem, idx) => [func(elem), idx])
+            .reduce((acc, curr) => (acc[0] < curr[0] ? acc : curr))[1];
+}
+
+const nearestPlane = (lat, lon) => {
+    /*
+    Communicate with the OpenSky API to find the nearest plane
+
+    Arguments:
+        lat - Latitude position of interest
+        lon - Longitude position of interest
+    Returns:
+        nearst (JSON) - information about the nearest plane
+    */
+
+   
+
+}
+
 router.route("/nearest")
     .get((req, res) => {
         console.log(`GET /nearest?lat=${req.query.lat}&lon=${req.query.lon}`);
@@ -79,8 +107,67 @@ router.route("/nearest")
             return;
         }
 
-        res.status(200).send(valid[1]);
-        return;
+        // Valid coordinates provided ==> Make request to OpenSky
+        const lat = valid[1].lat
+        const lon = valid[1].lon
+
+        // Starting search radius at 2 degrees, max of 32 degrees
+        let searchRadius = 2;
+
+        // Reject promise if empty states array returned
+        //      - Result of search radius being too narrow
+        openSky.interceptors.response.use((response) => {
+            if (response.data.states.length == 0) {
+                searchRadius = searchRadius*2;
+                return Promise.reject();
+            } else {
+                return response;
+            }
+        })
+
+        // Retry at most 5 times (doubling search radius each time)
+        axiosRetry(openSky, {retries: 5})
+
+        // Construct coordinate boundaries for each retry
+        const latlonString = () => {
+            let lamin = Math.max(lat-searchRadius, -90);
+            let lomin = Math.max(lon-searchRadius, -180);
+            let lamax = Math.min(lat+searchRadius, 90);
+            let lomax = Math.min(lon+searchRadius, 180);
+            return `lamin=${lamin}&lomin=${lomin}&lamax=${lamax}&lomax=${lomax}`;
+        }
+        openSky.get(`/states/all?${latlonString()}`)
+            .then(response => response.data)
+            .then(data => {
+                let states = data.states;
+                const dist = (state) => Math.sqrt((state[5]-lon)**2 + (state[6]-lat)**2);
+                
+                const nearest = states[argMin(states, dist)];
+                const body = {
+                    icao24: nearest[0],
+                    callsign: nearest[1],
+                    origin_country: nearest[2],
+                    time_position: nearest[3],
+                    last_contact: nearest[4],
+                    longitude: nearest[5],
+                    latitude: nearest[6],
+                    baro_altitude: nearest[7],
+                    on_ground: nearest[8],
+                    velocity: nearest[9],
+                    true_track: nearest[10],
+                    vertical_rate: nearest[11],
+                    sensors: nearest[12],
+                    geo_altitude: nearest[13],
+                    squawk: nearest[14],
+                    spi: nearest[15],
+                    position_source: nearest[16]
+                }
+                res.status(200).send(body);
+                return;
+            })
+            .catch(err => {
+                console.log("Error in reques to to OpenSky:\n"+err);
+            })            
     })
 
 module.exports = router;
