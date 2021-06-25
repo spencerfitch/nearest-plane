6b3f0189a8a06181c163e41415d7d857b7f95425
spencerfitch <sfitch16@gmail.com>
2021-06-25 17:24:29 -0500

    Added icon to navbar

--- client/src/Navigation.js
+++ client/src/Navigation.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import {Container, Navbar, Nav} from 'react-bootstrap';
+import { Container, Navbar, Nav } from 'react-bootstrap';
+import { IconContext } from 'react-icons';
+import { FaPlane } from 'react-icons/fa';
 
 
 class Navigation extends React.Component {
     render() {
         return (
         <Navbar bg="light" variant="light">
-            <Container fluid bg="primary">
-                <Navbar.Brand href="/">Nearest Plane</Navbar.Brand>
+            <Container fluid>
+                <Navbar.Brand href="/" className="d-flex align-items-center">
+                    <IconContext.Provider value={{className: 'text-primary', size: '30', style: {marginRight: "5px"}}}>
+                        <FaPlane />
+                    </IconContext.Provider>
+                    <b>Nearest Plane</b>
+                </Navbar.Brand>
                 <Nav className="me-auto" defaultActiveKey={window.location.pathname}>
                     <Nav.Link href="/">Home</Nav.Link>
                     <Nav.Link href="/nearest">Nearest</Nav.Link>
