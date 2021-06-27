297c07b3fa33bf7b55410053bfaaf1e412108a63
spencerfitch <sfitch16@gmail.com>
2021-06-26 19:58:24 -0500

    Updated homepage content and added location disclaimer

--- client/src/BodyPages/Home.js
+++ client/src/BodyPages/Home.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import {Container, Row} from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
+
+import { IconContext } from 'react-icons/lib';
+import { BsExclamationCircle } from 'react-icons/bs';
 
 import PlaneAnimation from './PlaneAnimation';
 
@@ -9,19 +12,27 @@ class Home extends React.Component {
         <Container fluid className="h-100 d-flex align-items-center">
             <Row className="w-100" style={{zIndex: 1}}>
                 <Row className="align-items-center">
-                    <h4 className="text-center">Welcome to Nearest Plane!</h4>
-                    <p className="w-75 text-center m-auto mb-5">
-                        Ever wanted to learn more about the planes that are flying over your head every day?
+                    <h4 className="text-center fs-1">Welcome to Nearest Plane</h4>
+                    <p className="w-100 text-center m-auto mb-5 fs-4">
+                        Discover the world of airplanes flying right over your head
                         <br/>
-                        Nearest Plane provides you with real time information about the plane closest to your current location.
-                        <br/>
-                        Learn about the current altitude, model, country of origin, and more of the planes in the skies above and on the ground nearby.
+                        
+
                     </p>
                 </Row>
-                <Row>
+                <Row className="mt-3">
                     <a href="/nearest" className="btn btn-primary w-50 m-auto btn-lg text-white">
                         Find your Nearest Plane
                     </a>
+                    <Container fluid className="d-flex align-items-center justify-content-center">
+                        <IconContext.Provider value={{className:'fw-light', size: 15, style:{marginRight: '3px'}}}>
+                            
+                            <BsExclamationCircle />
+                        </IconContext.Provider>
+                        <span className="fw-light fs-6">
+                            Location access required
+                        </span>
+                    </Container>
                 </Row>
 
             </Row>
