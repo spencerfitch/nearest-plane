3e89b229103efd2590a7aa23399a196c94790d95
spencerfitch <sfitch16@gmail.com>
2021-07-11 17:55:37 -0500

    Added accept-language as valid header to fix CORS error when spoofing location on chrome

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,7 +1,7 @@
 module.exports = {
     cors: (_req, res, next) => {
         res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, accept-language");
         res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE");
         next();
     }
