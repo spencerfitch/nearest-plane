8ee2f3272c882c4b1953ae02192a8b92b0b72a93
spencerfitch <59584619+spencerfitch@users.noreply.github.com>
2021-09-25 17:19:42 -0500

    Updated navigation.js to functional component

--- client/src/components/navigation.js
+++ client/src/components/navigation.js
@@ -4,27 +4,28 @@ import { IconContext } from 'react-icons';
 import { FaPlane } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
-
-class Navigation extends React.Component {
-    render() {
-        return (
-        <Navbar bg="white" variant="light" style={{zIndex: 1}}>
-            <Container fluid>
-                <Navbar.Brand to="/" as={Link} className="d-flex align-items-center">
-                    <IconContext.Provider value={{className: 'text-primary', size: '30', style: {marginRight: "5px"}}}>
-                        <FaPlane />
-                    </IconContext.Provider>
-                    <b>Nearest Plane</b>
-                </Navbar.Brand>
-                <Nav className="me-auto" defaultActiveKey={window.location.pathname}>
-                    <Nav.Link href="/" to="/" as={Link}>Home</Nav.Link>
-                    <Nav.Link href="/nearest" to="/nearest" as={Link}>Nearest</Nav.Link>
-                    <Nav.Link href="/about" to="/about" as={Link}>About</Nav.Link>
-                </Nav>
-            </Container>
-        </Navbar>
-        )
-    }
-}
+const Navigation = () => (
+  <Navbar bg="white" variant="light" style={{zIndex: 1}}>
+    <Container fluid>
+      <Navbar.Brand to="/" as={Link} className="d-flex align-items-center">
+        <IconContext.Provider value={{className: 'text-primary', size: '30', style: {marginRight: "5px"}}}>
+          <FaPlane />
+        </IconContext.Provider>
+        <b>Nearest Plane</b>
+      </Navbar.Brand>
+      <Nav className="me-auto" defaultActiveKey={window.location.pathname}>
+        <Nav.Link href="/" to="/" as={Link}>
+          Home
+        </Nav.Link>
+        <Nav.Link href="/nearest" to="/nearest" as={Link}>
+          Nearest
+        </Nav.Link>
+        <Nav.Link href="/about" to="/about" as={Link}>
+          About
+        </Nav.Link>
+      </Nav>
+    </Container>
+  </Navbar>
+);
 
 export default Navigation;
