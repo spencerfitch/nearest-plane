b8273d2b5650a094d137e44aef36275b6789b50a
spencerfitch <59584619+spencerfitch@users.noreply.github.com>
2021-09-25 16:41:30 -0500

    Changed App to functional component

--- client/src/App.js
+++ client/src/App.js
@@ -7,45 +7,26 @@ import About from './BodyPages/About';
 import Nearest from './BodyPages/Nearest';
 import NotFound from './BodyPages/NotFound';
 
-class App extends React.Component {
-    constructor (props) {
-        super (props);
-        this.state = {
-            "lat": null,
-            "lon": null,
-            "nearest": null
-        }
-    }
-
-    render () {
-        return (
-            <Router hashType="slash">
-                <div className="vh-100 d-flex flex-column bg-light">
-                    <Navigation />
-
-                    <Switch>
-                        <Route exact path="/">
-                            <Home />
-                        </Route>
-                        <Route path="/nearest">
-                            <Nearest />
-                        </Route>
-                        <Route path="/about">
-                            <About />
-                        </Route>
-                        <Route path="*">
-                            <NotFound />
-                        </Route>
-                    </Switch>
-
-                </div>
-            </Router>
-            
-        )
-    }
-    
-}
-
-
+const App = () => (
+  <Router hashType="slash">
+    <div className="vh-100 d-flex flex-column bg-light">
+      <Navigation />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/nearest">
+          <Nearest />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+    </div>
+  </Router>    
+);
 
 export default App;
